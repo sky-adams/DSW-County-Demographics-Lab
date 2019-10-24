897cbaeb67d83d7131028faff3796492401c056d
sky-adams <skyadams@sbunified.org>
2019-10-24 16:53:55 -0700

    Update countyDemographicsUnfinished.py

--- countyDemographicsUnfinished.py
+++ countyDemographicsUnfinished.py
@@ -11,7 +11,7 @@ def main():
     print(state_with_most_counties(counties))
 
 def high_income_counties(counties):
-    """Return a list of the counties with a median household income over $90,000."""
+    """Return a LIST of the counties with a median household income over $90,000."""
     
 
 def lowest_median_income(counties):
@@ -20,23 +20,23 @@ def lowest_median_income(counties):
 
 def alphabetically_first_county(counties):
     """Return the county with the name that comes first alphabetically."""
-
-
-def county_most_under_18(counties):
-    """Return the name a county with the highest percent of under 18 year olds."""
+    #Hint: you can use < to compare strings in Python. ex) "cat" < "dog" gives the value True
 
     
 def percent_most_under_18(counties):
     """Return the highest percent of under 18 year olds."""    
     
 
+def county_most_under_18(counties):
+    """Return the name a county with the highest percent of under 18 year olds."""
+    
 def state_with_most_counties(counties):
     """Return a state that has the most counties."""
-    #Make a dictionary that has a key for each state and the values keep track of the number of counties in each state
+    #1. Make a dictionary that has a key for each state and the values keep track of the number of counties in each state
     
-    #Find the state in the dictionary with the most counties
+    #2. Find the state in the dictionary with the most counties
     
-    #Return the state with the most counties
+    #3. Return the state with the most counties
     
     
 def your_interesting_demographic_function(counties):
